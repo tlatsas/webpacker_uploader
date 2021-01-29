1ea55a978b2cd35edaa55e7ceb8b6bd3cf8ea879
Esteban Zapata Rojas <esteban.zapata@avlos.ai>
2021-01-29 21:53:18 +0000

    instance: Allow ignored extensions to be configurable at class level.

--- lib/webpacker_uploader/instance.rb
+++ lib/webpacker_uploader/instance.rb
@@ -3,28 +3,38 @@
 require "mime-types"
 
 class WebpackerUploader::Instance
-  # TODO make this configurable
-  IGNORE_EXTENSION = %w[.map].freeze
-  public_constant :IGNORE_EXTENSION
+  DEFAULT_IGNORE_EXTENSION = %w[.map].freeze
 
   cattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT)) }
+  cattr_accessor(:ignored_extensions) { %w[.map] }
 
   def manifest
     @manifest ||= WebpackerUploader::Manifest.new
   end
 
   def upload!(provider, prefix: nil)
+    extensions_to_ignore = if ignored_extensions.nil? || !ignored_extensions.is_a?(Array)
+                             logger.warning('Ignored extensions should be specified as an array.')
+                             logger.warning("Ignoring #{DEFAULT_IGNORE_EXTENSION} instead")
+
+                             DEFAULT_IGNORE_EXTENSION
+                           else
+                             ignored_extensions
+                           end
+
     manifest.assets.each do |name, js_path|
       path = js_path[1..-1]
       remote_path = "#{prefix}/#{path}" unless prefix.nil?
-
       file_path = Rails.root.join("public", path)
 
-      if name.end_with?(*IGNORE_EXTENSION)
+      if name.end_with?(*extensions_to_ignore)
         logger.info("Skipping: #{file_path}")
       else
         logger.info("Processing: #{file_path}")
-        provider.upload!(remote_path, file_path, content_type_for(path)) unless name.end_with?(*IGNORE_EXTENSION)
+
+        unless name.end_with?(*extensions_to_ignore)
+          provider.upload!(path, file_path, content_type_for(path))
+        end
       end
     end
   end
