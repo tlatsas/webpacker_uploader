de0aecf3be44c7bf80fcc550718216218f1017b6
Esteban Zapata Rojas <2417465+estebanz01@users.noreply.github.com>
2021-02-02 17:13:42 +0000

    feat: Simplify validation around ignored extensions.

--- lib/webpacker_uploader/instance.rb
+++ lib/webpacker_uploader/instance.rb
@@ -3,8 +3,6 @@
 require "mime-types"
 
 class WebpackerUploader::Instance
-  DEFAULT_IGNORE_EXTENSION = %w[.map].freeze
-
   cattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT)) }
   cattr_accessor(:ignored_extensions) { %w[.map] }
 
@@ -13,37 +11,34 @@ class WebpackerUploader::Instance
   end
 
   def upload!(provider, prefix: nil)
-    extensions_to_ignore =
-      if ignored_extensions.nil? || !ignored_extensions.is_a?(Array)
-        logger.warning("Ignored extensions should be specified as an array.")
-        logger.warning("Ignoring #{DEFAULT_IGNORE_EXTENSION} instead")
-
-        DEFAULT_IGNORE_EXTENSION
-      else
-        ignored_extensions
-      end
+    if ignored_extensions.nil?
+      raise ArgumentError, "Ignored extensions should be specified as an array"
+    end
 
     manifest.assets.each do |name, js_path|
       path = js_path[1..-1]
-      remote_path = "#{prefix}/#{path}" unless prefix.nil?
+
+      remote_path =
+        if prefix.nil?
+          path
+        else
+          "#{prefix}/#{path}"
+        end
+
       file_path = Rails.root.join("public", path)
 
-      if name.end_with?(*extensions_to_ignore)
+      if name.end_with?(*ignored_extensions)
         logger.info("Skipping: #{file_path}")
       else
         logger.info("Processing: #{file_path}")
 
-        unless name.end_with?(*extensions_to_ignore)
-          provider.upload!(path, file_path, content_type_for(path))
-        end
+        provider.upload!(remote_path, file_path, content_type_for(path))
       end
     end
   end
 
-  private
-
-    def content_type_for(file)
-      fallback = MIME::Types.type_for(file).first.content_type
-      Rack::Mime.mime_type(File.extname(file), fallback)
-    end
+  private def content_type_for(file)
+    fallback = MIME::Types.type_for(file).first.content_type
+    Rack::Mime.mime_type(File.extname(file), fallback)
+  end
 end
