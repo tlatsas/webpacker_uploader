776643556ea212fe8133f6147a02fb042401f18b
Esteban Zapata Rojas <2417465+estebanz01@users.noreply.github.com>
2021-02-02 16:02:38 +0000

    aws: provider: Rename from_ec2 to instance_profile.

--- lib/webpacker_uploader/providers/aws.rb
+++ lib/webpacker_uploader/providers/aws.rb
@@ -25,7 +25,7 @@ module WebpackerUploader
         def credentials(options)
           if options[:profile_name].present?
             ::Aws::SharedCredentials.new(profile_name: options[:profile_name])
-          elsif options.key?(:from_ec2) && options[:from_ec2]
+          elsif options.key?(:instance_profile) && options[:instance_profile]
             ::Aws::InstanceProfileCredentials.new
           else
             ::Aws::Credentials.new(options[:access_key_id], options[:secret_access_key])
