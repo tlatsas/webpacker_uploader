f8a4991a82993191ac9d3791df78be64022cbd09
Tasos Latsas <tlatsas@hey.com>
2021-03-06 19:21:43 +0200

    Add test for missing manifest.json

--- test/manifest_test.rb
+++ test/manifest_test.rb
@@ -7,10 +7,21 @@ class ManifestTest < Minitest::Test
     @manifest = ::WebpackerUploader::Manifest.new
   end
 
+  def teardown
+    WebpackerUploader.reset!
+  end
+
   def test_assets
     assert_includes @manifest.assets, "application.css"
     assert_includes @manifest.assets, "application.js"
     assert_includes @manifest.assets, "application.png"
     refute_includes @manifest.assets, "entrypoints"
+  end
+
+  def test_missing_manifest
+    WebpackerUploader.config.public_manifest_path = "missing.json"
+    @empty_manifest = WebpackerUploader::Manifest.new
+
+    assert_empty @empty_manifest.assets
   end
 end
